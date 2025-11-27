08771de4791c3c3473a560ebb1f22a1fdb77ce03
Yanis <Yanis.Rahoui@etu.sorbonne-universite.fr>
2025-11-27 14:54:58 +0100

    Signal tap version

--- fit/DE10_Lite_Computer_YD/db/ip/slda7f67b63/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ fit/DE10_Lite_Computer_YD/db/ip/slda7f67b63/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b0e28be14ed51c55f183
+//   DESIGN_HASH   : fe0701e2de662ae67e80
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb0e28be14ed51c55f183 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfe0701e2de662ae67e80 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
